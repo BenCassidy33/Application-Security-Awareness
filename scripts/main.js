ea014818a464142ca5ff55bee1649defbfb102b1
Ben <ben.d.cassidy@gmail.com>
2024-12-01 22:53:18 -0500

    done for the day

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,20 @@ import { constants } from "../scripts/constants.js"
 const darkModeToggleButton = document.getElementById("lightDarkToggleButton")
 const root = document.querySelector(':root')
 
+const image = document.getElementById("parallax-img");
+
+const scrollImg = document.getElementById("content-image");
+const scrollBounds = document.getElementById("scroll-bounds");
+const scrollImgStart = document.getElementsByClassName("talking-point")[0]
+scrollImg.style.transform = `translateY(${scrollImgStart.clientHeight / 2}px)`
+
+let windowCenter = { x: window.innerWidth / 2, y: window.scrollY + (window.innerHeight / 2) }
+
+window.addEventListener('scroll', function() {
+    windowCenter.y = window.scrollY + (window.innerHeight / 2)
+
+})
+
 let toggleState = 0
 darkModeToggleButton.onclick = () => {
     toggleState = 1 - toggleState
@@ -21,41 +35,75 @@ darkModeToggleButton.onclick = () => {
     }
 }
 
-function addTestBlock(top, left) {
-    let block = document.getElementById('test-block')
 
-    block.style.display = "block"
-    block.style.top = `${top}px`
-    block.style.left = `${left ? left : window.innerWidth / 2}px`
+function addTestBlock(top = windowCenter.y, left = windowCenter.x, border = 0, padding = { x: 0, y: 0 }) {
+    let container = document.getElementById('testing')
+    let text = document.getElementById('test-text')
+
+    container.style.display = "flex"
+    container.style.top = `${top}px`
+    container.style.left = `${left}px`
+    container.style.border = `${border}px solid white`
+    container.style.padding = `${padding.y}px ${padding.x}px ${padding.y}px ${padding.x}px`
+    //container.style.transform = `translate(${left}px, ${top}px)`
+    text.innerText = `X: ${left}, Y: ${top}`
 }
 
 
-// TODO: impliment scrolling behaviour
-
 window.addEventListener('scroll', () => {
-    let scrollPos = window.scrollY;
-    let contentContainer = document.getElementById("content-image")
-    let scrollBounds = document.getElementById("scroll-bounds");
-    let bottomBound = scrollBounds.offsetTop + scrollBounds.clientHeight
+    //let centerPos = windowCenter.y;
+    let centerPos = window.scrollY + window.innerHeight / 2;
+    if (windowCenter.y < scrollBounds.offsetTop) return;
+    let styles = getComputedStyle(scrollBounds)
 
-    contentContainer.style.transform = `translateY(${Math.min(scrollPos, bottomBound)}px)`
+    scrollImg.style.transform = `translateY(${centerPos - scrollBounds.offsetTop}px)`
+
+    console.log(scrollBounds.clientHeight - scrollImg.clientHeight)
+    if (windowCenter.y + scrollImg.clientHeight > scrollBounds.offsetTop + scrollBounds.clientHeight) {
+        scrollImg.style.transform = `translateY(${scrollBounds.clientHeight - scrollImg.clientHeight}px)`
+
+    }
 })
 
 
-window.addEventListener('scroll', () => {
-    let scrollPos = window.scrollY + window.innerHeight / 2;
+const talkingPoints = document.getElementsByClassName("talking-point")
 
-    let image = document.getElementById("parallax-img")
+window.addEventListener('scroll', function() {
+    for (const talkingPoint of talkingPoints) {
+        let elemHeight = talkingPoint.clientHeight
+        let elemCenter = talkingPoint.offsetTop + elemHeight / 2
+        let maxRange = elemHeight * 2
+
+        const distance = Math.abs(windowCenter.y - elemCenter);
+        if (distance <= maxRange) {
+            const normalizedDistance = distance / maxRange;
+            const brightness = 1 - normalizedDistance;
+
+            talkingPoint.style.opacity = brightness;
+            talkingPoint.style.filter = `brightness(${brightness * 1.2})`;
+        } else {
+            talkingPoint.style.opacity = 0.2;
+            talkingPoint.style.filter = 'brightness(0.2)';
+        }
+    }
+})
+
+
+
+window.addEventListener('scroll', () => {
+    let scrollPos = windowCenter.y;
     let imageCenter = image.offsetTop + image.clientHeight / 2
 
     if (scrollPos < imageCenter) return;
     let dy = (scrollPos - imageCenter)
     let ds = 0.4 + dy * 0.0005
-    console.log(dy)
 
-    image.style.transform = `translateY(${dy}px) scale(${Math.min(1, ds)})`
+    image.style.transform = `translateY(${dy}px) scale(${Math.min(1.5, ds)})`
 
     if (dy > 5000) {
-        image.style.transform = `translateY(5000px)`
+        image.style.transform = `translateY(5000px) scale(1.5)`
     }
 })
+
+document.getElementById("parallax-range").style.marginTop = "13200px"
+
